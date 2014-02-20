b87e65304f1344a0494f5c278fc3b5cef67c40f3
leiting <brianlay@gmail.com>
2014-02-20 14:43:16 +0800

    1

--- test/src/test/web/URLTest.java
+++ test/src/test/web/URLTest.java
@@ -1,24 +1,13 @@
 package test.web;
 
 import java.io.BufferedReader;
-
 import java.io.IOException;
-
 import java.io.InputStream;
-
 import java.io.InputStreamReader;
-
 import java.net.Authenticator;
-
 import java.net.HttpURLConnection;
-
 import java.net.PasswordAuthentication;
-
 import java.net.URL;
-
-import java.net.URLConnection;
-
-import java.util.Properties;
 
 public class URLTest {
 
