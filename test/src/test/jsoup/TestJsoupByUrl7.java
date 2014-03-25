3ed3cae11e7683f31e2432e5d920a504a215d597
leiting <brianlay@gmail.com>
2014-03-25 13:43:03 +0800

    1

---
+++ test/src/test/jsoup/TestJsoupByUrl7.java
@@ -0,0 +1,97 @@
+package test.jsoup;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * @author Administrator 1911新中华
+ */
+public class TestJsoupByUrl7 {
+
+	private static boolean first = true;
+
+	private static final String titleRedundant = "_思路客";
+
+	private static boolean isSysout = false;
+
+	private static void handleRef(FileOutputStream fos, String url)
+			throws Exception {
+		Connection connection = Jsoup.connect(url);
+		connection.timeout(120000);
+		Document doc = connection.get();
+		String title = doc.title();
+		Element body = doc.body();
+		Element content = body.getElementById("main");
+		Element contents = content.getElementById("content");
+		String linkText = contents.text();
+		char c = linkText.charAt(0);
+		char[] cs = new char[4];
+		Arrays.fill(cs, c);
+		String s = new String(cs);
+		linkText = linkText.replaceAll(s, "\r\n\r\n");
+		if (first) {
+			first = false;
+		} else {
+			fos.write(("\r\n\r\n-------------------------------\r\n\r\n")
+					.getBytes("GBK"));
+		}
+
+		title = title.replaceAll(titleRedundant, "");
+		fos.write(title.getBytes("GBK"));
+		fos.write(linkText.getBytes("GBK"));
+	}
+
+	private static void handleDir(String url, String desDir) throws IOException {
+		FileOutputStream fos = null;
+		try {
+			Document doc = Jsoup.connect(url).get();
+			String title = doc.title();
+			title = title.replaceAll(titleRedundant, "");
+			fos = new FileOutputStream(new File(desDir + title + ".txt"));
+			Element body = doc.body();
+			Element content = body.getElementById("indexmain");
+			Elements links = content.getElementsByTag("a");
+			for (Element link : links) {
+				String linkHref = link.attr("href");
+				String linkText = link.text();
+				int index1 = linkText.indexOf("第");
+				int index2 = linkText.indexOf("章");
+				if (index1 >= 0 && index2 > 0 && index1 < index2) {
+					if (isSysout) {
+						System.out.println("linkHref:" + linkHref
+								+ " ; linkText:" + linkText);
+					}
+					String refUrl = url + linkHref;
+					if (isSysout) {
+						System.out.println(refUrl);
+					}
+					handleRef(fos, refUrl);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (fos != null) {
+				fos.close();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		String desDir = "e:\\books\\";
+
+		String url = "http://www.siluke.com/0/4/4948/";
+		handleDir(url, desDir);
+
+		// String url = "http://www.23us.com/html/9/9362/2660200.html";
+		// handleFile(url, desDir);
+	}
+}
